c305e3e3045836b2d6cbfe30f49e8978a42d88fe
fatimahaider786 <aimanfatima888786@gmail.com>
2025-11-02 00:43:00 +0500

    lec 13

---
+++ lec 13/index.js
@@ -0,0 +1,42 @@
+function getChatbot() {
+    const inputRef = document.querySelector("#query")
+    const query = inputRef.value
+    
+    const resultDiv = document.querySelector("#result")
+
+
+    const res = axios.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+        {
+            "contents": [
+                {
+                    "parts": [
+           {
+                            "text": "You are a friendly Cat Expert Chatbot named 'Snow who only answers questions about domestic cats. Answer in Roman Urdu, clearly and politely. Topics allowed: cat care (feeding, grooming), behaviour (scratching, meowing, litter training), basic health signs (when to see a vet), common breeds, toys and enrichment, and safe home setup. Give short practical tips (1–4 sentences) and one simple actionable step when possible. If the user asks for veterinary diagnosis, medical prescriptions, or emergencies, politely refuse and advise seeing a vet immediately. If asked about non-cat topics, refuse and gently steer back to cats. Keep tone warm, encouraging, and simple.",
+                        },
+                        {
+                            "text": query,
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            headers: {
+                "Content-Type": " application/json",
+                "x-goog-api-key": "AIzaSyDELYR0SBXEH2Et_FNrEKyMyvYylEK-37s",
+            }
+        }
+
+    )
+    console.log(res)
+
+    res
+        .then((data) => {
+            const answer = data.data.candidates[0].content.parts[0].text
+            resultDiv.innerHTML = answer
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+}
